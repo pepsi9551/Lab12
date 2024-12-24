58192891b14bffbad2660c4eaa26653affdc295f
pepsi9551 <0nuttawat0@gmail.com>
2024-12-24 16:17:14 +0700

    Push lab12_4.cpp

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &, T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,10 @@ int main(){
 	return 0;
 }
 
+template <typename Yeah>
+void mySwap(Yeah &x, Yeah &y){
+	Yeah i;
+	i = x;
+	x = y;
+	y = i;
+}
